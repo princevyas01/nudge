44f945575b8a123371ead44ff91dd2cec908fdfc
Prince Vyas <princevyas13456@gmail.com>
2026-09-13 12:16:50 +0530

    feat(domain): add smart_suggestion entity

---
+++ lib/domain/entities/smart_suggestion.dart
@@ -0,0 +1,17 @@
+﻿class SmartSuggestion {
+  final String id;
+  final String title;
+  final String description;
+  final String reminderId;
+  final DateTime suggestedTime;
+  final String actionType;
+
+  const SmartSuggestion({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.reminderId,
+    required this.suggestedTime,
+    required this.actionType,
+  });
+}
