e67d587f1c1b618abc7d62b2425bb2d3a1c9d5fb
Prince Vyas <princevyas13456@gmail.com>
2026-09-13 12:12:52 +0530

    feat(domain): add checklist_item entity

---
+++ lib/domain/entities/checklist_item.dart
@@ -0,0 +1,47 @@
+﻿import 'package:uuid/uuid.dart';
+
+class ChecklistItem {
+  final String id;
+  final String text;
+  final bool isDone;
+
+  const ChecklistItem({
+    required this.id,
+    required this.text,
+    this.isDone = false,
+  });
+
+  factory ChecklistItem.create({required String text, bool isDone = false}) {
+    return ChecklistItem(
+      id: const Uuid().v4(),
+      text: text,
+      isDone: isDone,
+    );
+  }
+
+  ChecklistItem copyWith({
+    String? id,
+    String? text,
+    bool? isDone,
+  }) {
+    return ChecklistItem(
+      id: id ?? this.id,
+      text: text ?? this.text,
+      isDone: isDone ?? this.isDone,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'text': text,
+    'isDone': isDone ? 1 : 0,
+  };
+
+  factory ChecklistItem.fromJson(Map<String, dynamic> json) {
+    return ChecklistItem(
+      id: json['id'] as String? ?? const Uuid().v4(),
+      text: json['text'] as String? ?? '',
+      isDone: json['isDone'] == 1 || json['isDone'] == true,
+    );
+  }
+}
