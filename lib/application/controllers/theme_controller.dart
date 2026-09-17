8f5d26fc064d22ed1044e0d96c1fe7e237c2999e
Prince Vyas <princevyas13456@gmail.com>
2026-09-17 19:31:05 +0530

    feat(application): add dynamic theme controller

---
+++ lib/application/controllers/theme_controller.dart
@@ -0,0 +1,40 @@
+﻿import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../../core/constants/app_constants.dart';
+
+class ThemeController extends ChangeNotifier {
+  ThemeMode _themeMode = ThemeMode.system;
+  ThemeMode get themeMode => _themeMode;
+
+  bool get isDarkMode => _themeMode == ThemeMode.dark;
+
+  ThemeController() {
+    _loadTheme();
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final savedMode = prefs.getString(AppConstants.prefThemeMode);
+    if (savedMode != null) {
+      if (savedMode == 'light') _themeMode = ThemeMode.light;
+      if (savedMode == 'dark') _themeMode = ThemeMode.dark;
+      if (savedMode == 'system') _themeMode = ThemeMode.system;
+      notifyListeners();
+    }
+  }
+
+  Future<void> setThemeMode(ThemeMode mode) async {
+    _themeMode = mode;
+    notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(AppConstants.prefThemeMode, mode.name);
+  }
+
+  Future<void> toggleTheme() async {
+    if (_themeMode == ThemeMode.dark) {
+      await setThemeMode(ThemeMode.light);
+    } else {
+      await setThemeMode(ThemeMode.dark);
+    }
+  }
+}
