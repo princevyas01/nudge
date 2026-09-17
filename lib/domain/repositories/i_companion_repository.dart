b02e7fc5c30592b0afef745efd48df987068773a
Prince Vyas <princevyas13456@gmail.com>
2026-09-17 19:17:41 +0530

    feat(domain): add companion repository contract

---
+++ lib/domain/repositories/i_companion_repository.dart
@@ -0,0 +1,7 @@
+﻿import '../entities/companion_profile.dart';
+
+abstract class ICompanionRepository {
+  Future<CompanionProfile> getProfile();
+  Future<void> saveProfile(CompanionProfile profile);
+  Future<void> resetProfile();
+}
