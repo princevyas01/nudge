db4bb53bd912461e5646d7ff3feb52ca48bbf860
Prince Vyas <princevyas13456@gmail.com>
2026-09-17 19:20:03 +0530

    feat(domain): add reminder repository contract

---
+++ lib/domain/repositories/i_reminder_repository.dart
@@ -0,0 +1,10 @@
+﻿import '../entities/reminder.dart';
+
+abstract class IReminderRepository {
+  Future<List<Reminder>> getAllReminders();
+  Future<Reminder?> getReminderById(String id);
+  Future<void> saveReminder(Reminder reminder);
+  Future<void> updateReminder(Reminder reminder);
+  Future<void> deleteReminder(String id);
+  Future<void> clearAllReminders();
+}
