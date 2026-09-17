0ba759a79278c372c2162b18ca11a2f250a95c6f
Prince Vyas <princevyas13456@gmail.com>
2026-09-17 19:20:50 +0530

    feat(domain): add template repository contract

---
+++ lib/domain/repositories/i_template_repository.dart
@@ -0,0 +1,8 @@
+﻿import '../entities/reminder_template.dart';
+
+abstract class ITemplateRepository {
+  Future<List<ReminderTemplate>> getAllTemplates();
+  Future<void> saveTemplate(ReminderTemplate template);
+  Future<void> deleteTemplate(String id);
+  Future<void> clearTemplates();
+}
