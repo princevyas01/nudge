db37a1676591e4a8a68399cff604fc03c44e933b
Prince Vyas <princevyas13456@gmail.com>
2026-09-17 19:19:16 +0530

    feat(domain): add pending action repository contract

---
+++ lib/domain/repositories/i_pending_action_repository.dart
@@ -0,0 +1,8 @@
+﻿import '../entities/pending_action.dart';
+
+abstract class IPendingActionRepository {
+  Future<List<PendingAction>> getAllPendingActions();
+  Future<void> addPendingAction(PendingAction action);
+  Future<void> removePendingAction(String id);
+  Future<void> clearPendingActions();
+}
