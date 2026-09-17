e5f8cbc0983491f0199b1bd32e9120237d78b11e
Prince Vyas <princevyas13456@gmail.com>
2026-09-17 19:18:05 +0530

    feat(domain): add folder repository contract

---
+++ lib/domain/repositories/i_folder_repository.dart
@@ -0,0 +1,10 @@
+﻿import '../entities/folder.dart';
+
+abstract class IFolderRepository {
+  Future<List<Folder>> getAllFolders();
+  Future<Folder?> getFolderById(String id);
+  Future<void> saveFolder(Folder folder);
+  Future<void> updateFolder(Folder folder);
+  Future<void> deleteFolder(String id, {required bool deleteContainedReminders});
+  Future<void> clearAllFolders();
+}
