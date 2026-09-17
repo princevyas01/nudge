9003f7d75d7cfd08e7d32d44acc88ceca8c73d31
Prince Vyas <princevyas13456@gmail.com>
2026-09-17 19:18:28 +0530

    feat(domain): add history repository contract

---
+++ lib/domain/repositories/i_history_repository.dart
@@ -0,0 +1,8 @@
+﻿import '../entities/reminder_history.dart';
+
+abstract class IHistoryRepository {
+  Future<List<ReminderHistory>> getAllHistory();
+  Future<List<ReminderHistory>> getHistoryForReminder(String reminderId);
+  Future<void> logEvent(ReminderHistory event);
+  Future<void> clearHistory();
+}
